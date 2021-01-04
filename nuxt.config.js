4a8309fc1baaacbc28a910a68ff14e7c4584cf9b
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2021-01-04 17:24:35 +0900

    Modify github actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,17 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/css-challenges/favicon.ico',
+      },
+    ],
+  },
+
+  router: {
+    base: '/css-challenges/',
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
