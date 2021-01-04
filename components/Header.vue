4bde0ea0a0e4ce559d9bd672b42a01cd1ec0f617
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2021-01-04 17:15:02 +0900

    Add top page

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav>
+      <div class="link">
+        <nuxt-link to="/">Home</nuxt-link>
+      </div>
+    </nav>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+nav {
+  background-color: rgb(164, 243, 127);
+  height: 10vh;
+}
+
+.link {
+  text-align: center;
+  padding-top: 2vh;
+  padding-left: 2vh;
+  display: flex;
+  justify-content: flex-start;
+}
+
+.link a {
+  font-size: 5vh;
+  color: #35495e;
+  text-decoration: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,14 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">100dayscss</h1>
+    <div class="title">100 Days CSS Challenges を解いていく</div>
+    <div class="list">
+      <div class="list-title">作ったもの</div>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <div v-for="s in solved" :key="s">
+          <nuxt-link :to="`/challenges/${s}`" class="button--green">{{
+            s
+          }}</nuxt-link>
+        </div>
       </div>
     </div>
   </div>
@@ -28,38 +17,88 @@
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      solved: [1],
+    };
+  },
+});
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
+  width: 90%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: flex-start;
   text-align: center;
+  flex-direction: column;
 }
 
+$title-size: 40px;
 .title {
+  font-size: $title-size;
+  color: #35495e;
+  border-bottom: solid 2px rgb(71, 70, 70);
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.list {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+$list-title-size: 25px;
+.list-title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: $list-title-size;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.list-title::after {
+  content: '';
+  display: inline-block;
+  position: relative;
+  width: $list-title-size * 2/3;
+  height: $list-title-size * 2/3;
+  bottom: 0;
+  border-top: $list-title-size/6 solid #b0b0b0;
+  border-right: $list-title-size/6 solid #b0b0b0;
+  transform: rotate(135deg) translate(-$list-title-size/2, -$list-title-size/4);
+  animation: 1.5s shake infinite ease-in;
+}
+
+@keyframes shake {
+  0% {
+    bottom: $list-title-size/3;
+  }
+  30% {
+    bottom: 0;
+  }
+  100% {
+    bottom: $list-title-size/3;
+  }
 }
 
 .links {
   padding-top: 15px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+.links > div {
+  margin: 1rem;
 }
 </style>
